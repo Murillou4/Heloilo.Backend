f2f591e288a98f99c4cd814d19a99adf10a1b097
Murillou4 <murillo003@hotmail.com>
2025-11-05 23:48:01 -0300

    Update assembly informational version and cache for Heloilo.Backend
    - Changed AssemblyInformationalVersion to reflect the latest build identifier.
    - Updated Heloilo.Backend.AssemblyInfoInputs.cache to ensure accurate build input tracking.

--- obj/Debug/net9.0/Heloilo.Backend.AssemblyInfo.cs
+++ obj/Debug/net9.0/Heloilo.Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Heloilo.Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd50fa29720aae189eef0e475cc7e57c9da384e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9487886c04279b8c38d4250a5a02eef29128926e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Heloilo.Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Heloilo.Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
