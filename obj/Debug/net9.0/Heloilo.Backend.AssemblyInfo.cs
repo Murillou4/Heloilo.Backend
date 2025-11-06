9487886c04279b8c38d4250a5a02eef29128926e
Murillou4 <murillo003@hotmail.com>
2025-11-05 23:47:30 -0300

    Update assembly information and project directory paths for Heloilo components
    - Updated AssemblyInformationalVersion attributes in Heloilo.Domain, Heloilo.Infrastructure, and Heloilo.Backend to reflect new versioning.
    - Modified project directory paths in generated MSBuild editor config files for consistency across platforms.
    - Updated cache files for assembly inputs and assets to ensure proper build configurations.

--- obj/Debug/net9.0/Heloilo.Backend.AssemblyInfo.cs
+++ obj/Debug/net9.0/Heloilo.Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Heloilo.Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b6326d5602d0250d2966f488c35958931e64f01")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd50fa29720aae189eef0e475cc7e57c9da384e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Heloilo.Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Heloilo.Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
